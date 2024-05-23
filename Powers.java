c979349147f1b14bf160d85f590ee13e16e53ba6
Raviteja-Gandham <168612928+Raviteja-Gandham@users.noreply.github.com>
2024-05-23 20:47:55 +0530

    Powers.java

---
+++ Powers.java
@@ -0,0 +1,28 @@
+/*
+print squares and cubes of 2,3,4
+output:
+4
+8
+9
+27
+16
+64
+*/
+public class Powers
+{
+public static void main(String args[])
+{
+	System.out.println((int)(Math.pow(2,2)));
+	System.out.println(Math.pow(2,3));
+	System.out.println(Math.pow(3,2));
+	System.out.println(Math.pow(3,3));
+	System.out.println(Math.pow(4,2));
+	System.out.println(Math.pow(4,3));
+	System.out.println((int)(Math.pow(2,2)));
+	System.out.println((int)(Math.pow(2,3)));
+	System.out.println((int)(Math.pow(3,2)));
+	System.out.println((int)(Math.pow(3,3)));
+	System.out.println((int)(Math.pow(4,2)));
+	System.out.println((int)(Math.pow(4,3)));
+}
+}
